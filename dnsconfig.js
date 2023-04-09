aec8c9e9a600249c34149fa83c564287a964cf22
Jonathan Beliën <dev@jbelien.be>
2023-04-09 17:54:53 +0200

    Initial commit

---
+++ dnsconfig.js
@@ -0,0 +1,118 @@
+// Providers:
+
+var REG_NONE = NewRegistrar("none"); // No registrar.
+var DSP_DIGITALOCEAN = NewDnsProvider("digitalocean");
+var DSP_TRANSIP = NewDnsProvider("transip");
+
+// Domains:
+
+D(
+  "osoc.be",
+
+  REG_NONE,
+  DnsProvider(DSP_DIGITALOCEAN),
+
+  DefaultTTL(3600),
+
+  A("@", "104.198.14.52"),
+  CNAME("www", "osoc.be.", TTL(43200)),
+
+  A("2011", "37.139.14.192"),
+  A("2012", "37.139.14.192"),
+  A("2013", "37.139.14.192"),
+  A("2014", "37.139.14.192"),
+  A("2015", "37.139.14.192"),
+  A("2016", "37.139.14.192"),
+  A("2017", "37.139.14.192"),
+  A("2018", "37.139.14.192"),
+  CNAME("2019", "redirect.openknowledge.be.", TTL(43200)),
+  CNAME("2020", "redirect.openknowledge.be.", TTL(43200)),
+
+  CNAME("help", "hosting.gitbook.com.", TTL(43200)),
+  CNAME("selections", "osoc-selection.netlify.app.", TTL(43200)),
+  CNAME("selection", "osoc-selection.netlify.app.", TTL(43200)),
+
+  CNAME("status", "page.updown.io.", TTL(43200)),
+  TXT("_updown.status", "updown-page=p/ysh0m"),
+
+  // Fastmail - https://www.fastmail.help/hc/en-us/articles/1500000280261
+  MX("@", 10, "in1-smtp.messagingengine.com.", TTL(14400)),
+  MX("@", 20, "in2-smtp.messagingengine.com.", TTL(14400)),
+
+  TXT("@", "google-site-verification=DPZt0TubKlpnBoeS379uL7w7QG5eviALnXhQRTF2TS8"),
+  TXT("_github-challenge-osoc22-org", "eb1ed9ede8"),
+  TXT("_github-challenge-osoc21-org", "ff9ba1ccbb"),
+  TXT("_github-challenge-osoc20-org", "395e0c9ad8"),
+  TXT("_github-challenge-opensummerofcode-org", "c3a84b2b80"),
+  TXT("_github-pages-challenge-opensummerofcode", "d199c379417ca92a0d1e3eef15b6ec"),
+  TXT("_vercel", "vc-domain-verify=pleasant-procedures.osoc.be,bb2ed95aab71d6163a58"),
+
+  // Projects 2019
+  A("api.cyclingup", "82.196.15.97"),
+  A("cyclingup", "82.196.15.97"),
+  A("cycling-up", "82.196.15.97"),
+
+  // Projects 2021
+  A("ldes", "178.128.242.87"),
+  A("future-of-work", "167.71.72.187"),
+  A("roadbase", "164.90.192.163"),
+  CNAME("api.crowdy", "comparative-eyrie-hyskunkkeyy6pa2vm3l49atd.herokudns.com.", TTL(43200)),
+  CNAME("crowdy", "closed-locust-t0qafbawjox1vvvo32wczzaw.herokudns.com.", TTL(43200)),
+  CNAME("wepod", "osoc21.github.io.", TTL(43200)),
+
+  // Projects 2022
+  A("api.inspector-widget", "161.35.150.132"),
+  CNAME("idlab", "seal-app-rgoxe.ondigitalocean.app.", TTL(1800)),
+  CNAME("api.knowledge-impact", "the-impact-of-sharing-knowledge-5nvs7.ondigitalocean.app.", TTL(1800)),
+  CNAME("paradar", "cname.vercel-dns.com.", TTL(43200)),
+  CNAME("pleasant-procedures", "cname.vercel-dns.com.", TTL(43200))
+);
+
+D(
+  "opensummerofcode.be",
+
+  REG_NONE,
+  DnsProvider(DSP_DIGITALOCEAN),
+
+  DefaultTTL(3600),
+
+  A("@", "188.166.35.76"),
+  CNAME("www", "redirect.openknowledge.be.", TTL(43200)),
+
+  CNAME("2011", "redirect.openknowledge.be.", TTL(43200)),
+  CNAME("2012", "redirect.openknowledge.be.", TTL(43200)),
+  CNAME("2013", "redirect.openknowledge.be.", TTL(43200)),
+  CNAME("2014", "redirect.openknowledge.be.", TTL(43200)),
+  CNAME("2015", "redirect.openknowledge.be.", TTL(43200)),
+  CNAME("2016", "redirect.openknowledge.be.", TTL(43200)),
+  CNAME("2017", "redirect.openknowledge.be.", TTL(43200)),
+  CNAME("2018", "redirect.openknowledge.be.", TTL(43200)),
+  CNAME("2019", "redirect.openknowledge.be.", TTL(43200)),
+  CNAME("2020", "redirect.openknowledge.be.", TTL(43200))
+);
+
+D(
+  "summerofcode.be",
+
+  REG_NONE,
+  DnsProvider(DSP_DIGITALOCEAN),
+
+  DefaultTTL(3600),
+
+  A("@", "188.166.35.76"),
+  CNAME("www", "redirect.openknowledge.be.", TTL(43200)),
+
+  A("ttl", "37.139.14.192"),
+
+  CNAME("2011", "redirect.openknowledge.be.", TTL(43200)),
+  CNAME("2012", "redirect.openknowledge.be.", TTL(43200)),
+  CNAME("2013", "redirect.openknowledge.be.", TTL(43200)),
+  CNAME("2014", "redirect.openknowledge.be.", TTL(43200)),
+  CNAME("2015", "redirect.openknowledge.be.", TTL(43200)),
+  CNAME("2016", "redirect.openknowledge.be.", TTL(43200)),
+  CNAME("2017", "redirect.openknowledge.be.", TTL(43200)),
+  CNAME("2018", "redirect.openknowledge.be.", TTL(43200)),
+  CNAME("2019", "redirect.openknowledge.be.", TTL(43200)),
+  CNAME("2020", "redirect.openknowledge.be.", TTL(43200)),
+  CNAME("open", "redirect.openknowledge.be.", TTL(43200))
+);
